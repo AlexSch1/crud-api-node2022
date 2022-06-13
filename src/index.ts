005f94e03096403bcf345fad794e1d27667868fe
Aleksey Shulyak <a.shulyak@itransition.com>
2022-06-13 16:10:22 +0400

    feature: Add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config'
 
-console.log(process.env['NODE_ENV'])
-console.log(process.env['PORT'])
+// console.log(process.env.NODE_ENV)
+console.log(process.env.PORT)
 
 function sayMyName(name: string): void {
     if (name === "Heisenberg") {
-        console.log("You're right 👍");
+        // console.log("You're right 👍");
     } else {
-        console.log("You're wrong 👎");
+        // console.log("You're wrong 👎");
     }
 }
 
